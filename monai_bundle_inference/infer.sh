54d0577987c51f150fba1e901f0d1eaf57095b54
Suraj Pai <bspai@bwh.harvard.edu>
2023-03-14 06:24:51 -0400

    REST API allows direct load of commands on docker

--- monai_bundle_inference/infer.sh
+++
@@ -1,7 +0,0 @@
-# Bundle script to run inference on custom data
-python -m monai.bundle run inference \
-  --meta_file configs/metadata.json \
-  --config_file configs/inference.yaml \
-  --logging_file configs/logging.conf \
-  --datadir /app/data/input \
-  --output_dir /app/data/output
